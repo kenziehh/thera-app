2ad495e9764efb9c8276b3f5bb6a4d9c9ab909f3
kenziehh <ghaisani.nurani@gmail.com>
2025-05-31 18:49:52 +0700

    feat: hide topbar login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout() {
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
+          <Stack.Screen name="login" options={{ headerShown: false }} />
         </Stack>
         <StatusBar style="auto" />
       </ReactQueryProvider>
--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,5 +1,13 @@
 import LoginContainer from "@/features/auth/containers/LoginContainer";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
 
 export default function SignInScreen() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
+  
   return <LoginContainer />
 }
